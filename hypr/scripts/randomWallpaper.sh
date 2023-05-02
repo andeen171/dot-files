d7f4a6e1cb1bf8652895efa4128c422b66b4263b
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2023-05-02 12:18:06 -0300

    Update again

--- hypr/scripts/randomWallpaper.sh
+++ hypr/scripts/randomWallpaper.sh
@@ -2,6 +2,6 @@
 while true
 do
     PIC=$(find -L ~/Pictures/Wallpapers/img/ -type f | shuf -n 1)
-    swww img "$PIC"
+    swww img --transition-type wipe --transition-angle 30 --transition-step 90 "$PIC"
     sleep 5m
 done
