c37d259b9535d378ac870199726911523791d053
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2023-05-08 13:49:21 -0300

    Update fish config

---
+++ fish/conf.d/set_cargo_path.fish
@@ -0,0 +1,1 @@
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    unimatrix -a -f -l k -s 100 -w
-    neofetch
+    unimatrix -a -f -l o -s 100 -w -c magenta
+    neofetch --ascii_colors 5 1 --ascii_bold on --colors 5 1 5 1 5 
 end
 set fish_greeting
+fish_add_path /home/andeen/.spicetify
